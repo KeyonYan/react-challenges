ce271b25de3769db2996d84eaafc811b28b2abcc
KeyonYan <835254227@qq.com>
2023-12-10 23:34:54 +0800

    feat: day1 - Show/Hide title

---
+++ app/day1/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useState } from "react"
+
+export default function Day1() {
+  const [show, setShow] = useState(false)
+  return (
+    <div className="p-4 flex flex-col gap-4">
+      <h1 className="text-xl font-bold">Day 1</h1>
+
+      <div className="flex flex-row gap-2">
+        <button onClick={() => setShow(!show)} className="text-lg rounded-lg p-1 bg-slate-300">Show / Hide</button>
+        {show && <div className="text-2xl font-bold">Welcome to React Challenges</div>}
+      </div>
+    </div>
+  )
+}
