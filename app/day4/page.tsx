7f166880ad58b2914269d7331fb75b650d5445e1
KeyonYan <835254227@qq.com>
2023-12-11 23:21:00 +0800

    feat: day4 - ProgressBar

---
+++ app/day4/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { ChangeEventHandler, useState } from "react"
+import ProgressBar from "@/components/ProgressBar"
+
+export default function Day4() {
+  const [value, setValue] = useState(50)
+  const handleChange: ChangeEventHandler<HTMLInputElement>= (e) => {
+    const val = Number(e.target.value)
+    setValue(Math.max(0, Math.min(val, 100)))
+  }
+  return (
+    <div className="p-4 flex flex-col gap-4">
+      <h1 className="text-xl font-bold">Day 4</h1>
+      <div className="text-2xl font-bold">Progress Bar</div>
+      <ProgressBar value={value} max={100}/>
+      <div className="flex flex-row gap-4 items-center">
+        <div>Input Precentage: </div>
+        <input onChange={handleChange} className="rounded-lg p-1 border-2" placeholder="progress value" value={value} min='0' max='100' type='number'></input>
+      </div>
+    </div>
+  )
+}
---
+++ components/ProgressBar.tsx
@@ -0,0 +1,43 @@
+import { useRef, useState, useEffect } from "react"
+import { useDebounce } from "@uidotdev/usehooks";
+import styles from './ProgressBar.module.css'
+
+interface InnerProgressValueProps {
+  value: number
+  max: number
+}
+
+function InnerProgressValue( props: InnerProgressValueProps) {
+  const { value, max } = props
+  const [hidden, setHidden] = useState(true)
+  const valueElement = useRef<HTMLDivElement>(null)
+  const innerProgressElement = useRef<HTMLDivElement>(null)
+  const debouncedUpdateValue = useDebounce(value, 300);
+  useEffect(() => {
+    if (debouncedUpdateValue) {
+      const outerElementWidth = innerProgressElement.current?.offsetWidth ?? 0
+      const textElementWidth = valueElement.current?.offsetWidth ?? 0
+      if (!hidden && outerElementWidth <= textElementWidth + 10) {
+        setHidden(true)
+      } else if (hidden && outerElementWidth > textElementWidth + 10) {
+        setHidden(false)
+      }
+      console.log(value)
+    }
+  }, [debouncedUpdateValue])
+
+  return (
+    <div ref={innerProgressElement} className={`${styles.animateGradientBg} transition-width duration-500 ease-in-out text-lg h-7 text-white flex justify-center rounded-lg bg-gradient-to-r from-[#ff9a9e] to-[#fad0c4]`} style={{ width: `${value/max*100}%`}}>
+      <div className={`${hidden ? 'opacity-0' : 'opacity-100 w-auto'} w-auto transition-opacity duration-1000 ease-in-out`} ref={valueElement}>{value}%</div>
+    </div>
+  )
+}
+
+export default function ProgressBar(props: InnerProgressValueProps) {
+  let { value, max } = props
+  return (
+    <div className='rounded-lg border-2 bg-[#cdcdcd] shadow-lg h-8'>
+      <InnerProgressValue value={value} max={max} />
+    </div>
+  )
+}
