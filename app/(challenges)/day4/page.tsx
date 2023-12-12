e36d3d5c6bfe33d11eeeac5f3a3d947312a01f87
KeyonYan <835254227@qq.com>
2023-12-13 01:33:53 +0800

    ✨ feat: 添加变体样式选择框

--- app/(challenges)/day4/page.tsx
+++ app/(challenges)/day4/page.tsx
@@ -2,20 +2,32 @@
 import { ChangeEventHandler, useState } from "react"
 import Progress from "@/components/ProgressBar"
 
+type Variant = "default" | "bright" | "dark" | null | undefined
+
 export default function Day4() {
   const [value, setValue] = useState(50)
-  const handleChange: ChangeEventHandler<HTMLInputElement>= (e) => {
+  const [variant, setVariant] = useState<Variant>('default')
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     const val = Number(e.target.value)
     setValue(Math.max(0, Math.min(val, 100)))
+  }
+  const handleChangeVariant: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    const val = e.target.value as Variant
+    setVariant(val)
   }
   return (
     <div className="p-4 flex flex-col gap-4">
       <h1 className="text-xl font-bold">Day 4</h1>
       <div className="text-2xl font-bold">Progress Bar</div>
-      <Progress value={value} max={100} size='lg' variant='bright'/>
+      <Progress value={value} max={100} size='lg' variant={variant}/>
       <div className="flex flex-row gap-4 items-center">
         <div>Input Precentage: </div>
         <input onChange={handleChange} className="rounded-lg p-1 border-2" placeholder="progress value" value={value} min='0' max='100' type='number'></input>
+        <select title="variant Style" value={variant as string} onChange={handleChangeVariant} className="rounded-lg p-1 border-2">
+          <option selected value="default">default</option>
+          <option value="bright">bright</option>
+          <option value="dark">dark</option>
+        </select>
       </div>
     </div>
   )
