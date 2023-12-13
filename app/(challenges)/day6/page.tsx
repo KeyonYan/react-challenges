b671e90173e270f159ac3fc9ce41343f9aa5b8d3
KeyonYan <835254227@qq.com>
2023-12-13 22:11:36 +0800

    ✨ feat: Resizable Card

--- app/(challenges)/day6/page.tsx
+++ app/(challenges)/day6/page.tsx
@@ -1,12 +1,15 @@
 'use client'
-import Card from "@/components/Card/Card";
+import {ResizableBox, Card} from "@/components/Card/Card";
+import { useState } from "react";
 
 const Day6 = () => {
+  const [cardHeight, setCardHeight] = useState(300)
+  const [cardWidth, setCardWidth] = useState(200)
   return (
     <div className="p-4 flex flex-col gap-4">
       <h1 className="text-xl font-bold">Day 6</h1>
-      <div className="text-2xl font-bold">Card</div>
-      <Card height={200} width={200} />
+      <div className="text-2xl font-bold">Resizable Card</div>
+      <ResizableBox className='w-screen h-screen' height={cardHeight} width={cardWidth} setHeight={setCardHeight} setWidth={setCardWidth} />
     </div>
   )
 }
--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,55 +1,41 @@
 import { cn } from "@/lib/utils"
-import { HTMLAttributes, MouseEventHandler, forwardRef, useState } from "react"
+import { Dispatch, HTMLAttributes, MouseEventHandler, SetStateAction, forwardRef, useState } from "react"
+
+type Axis = 'x' | 'y' | 'xy'
 
 export interface CardHandleProps extends HTMLAttributes<HTMLDivElement> {
+  axis: Axis
   hover: boolean
-  height: number,
-  width: number
-  axis: 'x' | 'y' | 'xy'
-  onCardResize: (height: number, width: number) => void
+  onCardResize: MouseEventHandler<HTMLDivElement>
+  onCardResizeStart: MouseEventHandler<HTMLDivElement>
+  onCardResizeEnd: MouseEventHandler<HTMLDivElement>
 }
 
 const CardHandle = forwardRef<HTMLDivElement, CardHandleProps>(
-  ({className, hover, height, width, onCardResize, axis, ...props}, ref) => {
-    const [isDragging, setIsDragging] = useState<boolean>(false);
-    const [mousePos, setMousePos] = useState<{x: number, y: number}>({x: 0, y: 0})
-    const handleResizeStart: MouseEventHandler<HTMLDivElement> = (e) => {
-      setIsDragging(true)
-      const newMousePos = {x: e.clientX, y: e.clientY}
-      setMousePos(newMousePos)
-      console.log("start: ", mousePos)
+  ({className, hover, onResize, onCardResize, onCardResizeStart, onCardResizeEnd, axis, ...props}, ref) => {
+    
+    const handleBoxStyle = `${axis.includes('y') ? 'bottom-0 w-1/2 h-8 cursor-row-resize' : ''} ${axis.includes('x') ? 'right-0 h-1/2 w-8 cursor-col-resize' : ''}`
+    const handleStyle = `${axis.includes('y') ? 'w-1/2 h-1' : ''} ${axis.includes('x') ? 'h-1/2 w-1' : ''}`
+    if (axis === 'xy') {
+      return (
+        <div
+          ref={ref}
+          onMouseMove={onCardResize}
+          onMouseDown={onCardResizeStart}
+          onMouseUp={onCardResizeEnd}
+          className={cn(hover ? 'opacity-100' : 'opacity-0', 'absolute bottom-0 right-0 flex justify-center items-center w-1/5 h-1/5')}
+        >
+          <div className='border-4 w-1/2 h-1/2 border-[#6f6f6f] rounded-br-lg'></div>
+          <div className='absolute mb-2 mr-2 border-1 bg-white w-1/2 h-1/2 rounded-br-lg'></div>
+        </div>
+      )
     }
-    const handleResizeEnd: MouseEventHandler<HTMLDivElement> = (e) => {
-      if (!isDragging) return
-      setIsDragging(false)
-      console.log("end: ", mousePos)
-    }
-    const handleResize: MouseEventHandler<HTMLDivElement> = (e) => {
-      if (!isDragging) return
-      console.log("resize: ", mousePos)
-      let diffX = 0, diffY = 0
-      if (axis.includes('y')) {
-        diffY = e.clientY - mousePos.y;
-      } else if (axis.includes('x')) {
-        diffX = e.clientX - mousePos.x;
-      }
-      console.log("diffY: ", diffY)
-      const newHeight = height + diffY;
-      const newWidth = width + diffX;
-      console.log("newHeight: ", newHeight)
-      console.log("newWidth: ", newWidth)
-      onCardResize(newHeight, newWidth); // Adjust minimum height
-      setMousePos({x: e.clientX, y: e.clientY})
-    }
-    const handleBoxStyle = `${axis.includes('y') ? 'bottom-0 w-full h-8 cursor-row-resize' : ''} ${axis.includes('x') ? 'right-0 h-full w-8 cursor-col-resize' : ''}`
-    const handleStyle = `${axis.includes('y') ? 'w-1/5 h-1' : ''} ${axis.includes('x') ? 'h-1/5 w-1' : ''}`
     return (
       <div
         ref={ref}
-        onMouseMove={handleResize}
-        onMouseDown={handleResizeStart}
-        onMouseUp={handleResizeEnd}
-        onMouseLeave={handleResizeEnd}
+        onMouseMove={onCardResize}
+        onMouseDown={onCardResizeStart}
+        onMouseUp={onCardResizeEnd}
         className={cn(hover ? 'opacity-100' : 'opacity-0', handleBoxStyle, 'absolute flex items-center justify-center')}
       >
         <div className={cn(handleStyle, 'transition-opacity duration-2000 ease-in rounded-lg bg-[#6f6f6f]')}></div>
@@ -61,31 +47,24 @@ const CardHandle = forwardRef<HTMLDivElement, CardHandleProps>(
 CardHandle.displayName = 'CardHandle'
 
 export interface CardProps extends HTMLAttributes<HTMLDivElement> {
-  height: number,
-  width: number,
+  height: number
+  width: number
+  hover: boolean
+  onCardHover: (hover: boolean) => void
 }
 
-const Card = forwardRef<HTMLDivElement, CardProps>(
-  ({ className, height, width, ...props}, ref) => {
-    const [cardHeight, setCardHeight] = useState<number>(height)
-    const [cardWidth, setCardWidth] = useState<number>(width)
-    const [hover, setHover] = useState<boolean>(false)
-    const handleCardResize = (height: number, width: number) => {
-      console.log("card - height: ", height)
-      setCardHeight(height)
-      setCardWidth(width)
-    }
+export const Card = forwardRef<HTMLDivElement, CardProps>(
+  ({ className, children, height, width, hover, onCardHover, ...props}, ref) => {
     return (
       <div
         ref={ref}
-        style={{height: cardHeight, width: cardWidth}}
+        style={{height: height, width: width}}
         className={cn('w-60 rounded-lg border bg-card text-card-foreground shadow-sm flex justify-center items-center relative', className)}
         {...props}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
+        onMouseEnter={() => onCardHover(true)}
+        onMouseLeave={() => onCardHover(false)}
       >
-        <CardHandle hover={hover} height={cardHeight} width={cardWidth} axis='y' onCardResize={handleCardResize}/>
-        <CardHandle hover={hover} height={cardHeight} width={cardWidth} axis='x' onCardResize={handleCardResize}/>
+        {children}
       </div>
     )
   }
@@ -93,4 +72,63 @@ const Card = forwardRef<HTMLDivElement, CardProps>(
 
 Card.displayName = 'Card'
 
-export default Card
+
+export interface ResizableBoxProps extends HTMLAttributes<HTMLDivElement> {
+  height: number
+  width: number
+  setHeight: (height: number) => void
+  setWidth: (width: number) => void
+}
+
+export const ResizableBox = forwardRef<HTMLDivElement, ResizableBoxProps>(
+  ({className, children, height, width, setHeight, setWidth, ...props}, ref) => {
+    const [hover, setHover] = useState<boolean>(false)
+    const [isDragging, setIsDragging] = useState<boolean>(false);
+    const [mousePos, setMousePos] = useState<{x: number, y: number}>({x: 0, y: 0})
+    const handleResizeStart: MouseEventHandler<HTMLDivElement> = (e) => {
+      setIsDragging(true)
+      const newMousePos = {x: e.clientX, y: e.clientY}
+      setMousePos(newMousePos)
+    }
+    const handleResizeEnd: MouseEventHandler<HTMLDivElement> = (e) => {
+      if (!isDragging) return
+      setIsDragging(false)
+    }
+    const handleHeightResize: MouseEventHandler<HTMLDivElement> = (e) => {
+      if (!isDragging) return
+      const diffY = e.clientY - mousePos.y
+      const newHeight = height + diffY;
+      setHeight(newHeight)
+      const newMousePos = {x: e.clientX, y: e.clientY}
+      setMousePos(newMousePos)
+    }
+    const handleWidthResize: MouseEventHandler<HTMLDivElement> = (e) => {
+      if (!isDragging) return
+      const diffX = e.clientX - mousePos.x;
+      const newWidth = width + diffX;
+      setWidth(newWidth)
+      const newMousePos = {x: e.clientX, y: e.clientY}
+      setMousePos(newMousePos)
+    }
+    const handleHWResize: MouseEventHandler<HTMLDivElement> = (e) => {
+      handleHeightResize(e)
+      handleWidthResize(e)
+    }
+    return (
+      <div className={cn(className)}>
+        <Card
+          height={height}
+          width={width}
+          hover={hover}
+          onCardHover={(h) => setHover(h)}
+        >
+          <CardHandle hover={hover} axis='y' onCardResize={handleHeightResize} onCardResizeStart={handleResizeStart} onCardResizeEnd={handleResizeEnd} />
+          <CardHandle hover={hover} axis='x' onCardResize={handleWidthResize} onCardResizeStart={handleResizeStart} onCardResizeEnd={handleResizeEnd} />
+          <CardHandle hover={hover} axis='xy' onCardResize={handleHWResize} onCardResizeStart={handleResizeStart} onCardResizeEnd={handleResizeEnd} />
+        </Card>
+      </div>
+    )
+  }
+)
+
+ResizableBox.displayName = 'ResizableBox'
