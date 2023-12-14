b2797f31d59d47946359c5bb168f3c1391c178d7
KeyonYan <835254227@qq.com>
2023-12-14 20:45:17 +0800

    🦄 refactor: 命名重构，功能解构

--- app/(challenges)/day6/page.tsx
+++ app/(challenges)/day6/page.tsx
@@ -1,25 +1,27 @@
 'use client'
-import {Card} from "@/components/Card/Card";
+import { ResizableCard, Card } from "@/components/Card/Card";
 import { useState } from "react";
 import { GitHubLogoIcon, DiscordLogoIcon, CodeSandboxLogoIcon } from '@radix-ui/react-icons'
 
 const Day6 = () => {
-  const [cardHeight, setCardHeight] = useState(300)
-  const [cardWidth, setCardWidth] = useState(200)
+  const [firstCardHeight, setFirstCardHeight] = useState(300)
+  const [firstCardWidth, setFirstCardWidth] = useState(200)
+  const [secondCardHeight, setSecondCardHeight] = useState(300)
+  const [thirdCardWidth, setThirdCardWidth] = useState(200)
   return (
     <div className="p-4 flex flex-col gap-4">
       <h1 className="text-xl font-bold">Day 6</h1>
       <div className="text-2xl font-bold">Resizable Card</div>
       <div className="flex flex-row gap-4">
-        <Card height={cardHeight} width={cardWidth} onChangeHeight={setCardHeight} onChangeWidth={setCardWidth}>
+        <ResizableCard height={firstCardHeight} width={firstCardWidth} onChangeHeight={setFirstCardHeight} onChangeWidth={setFirstCardWidth}>
           <GitHubLogoIcon className='w-10 h-10' />
-        </Card>
-        <Card height={300} width={200} className='bg-[#cbcdff]'>
+        </ResizableCard>
+        <ResizableCard height={secondCardHeight} width={200} onChangeHeight={setSecondCardHeight} className='bg-[#cbcdff]'>
           <DiscordLogoIcon className='w-10 h-10' color='#5560ee' />
-        </Card>
-        <Card height={300} width={200} className='bg-[#e4fc82]'>
+        </ResizableCard>
+        <ResizableCard height={300} width={thirdCardWidth} onChangeWidth={setThirdCardWidth} className='bg-[#e4fc82]'>
           <CodeSandboxLogoIcon className='w-10 h-10' />
-        </Card>
+        </ResizableCard>
       </div>
     </div>
   )
--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -4,28 +4,28 @@ import { MixerHorizontalIcon } from '@radix-ui/react-icons'
 
 type Axis = 'x' | 'y' | 'xy'
 
-export interface CardHandleProps extends HTMLAttributes<HTMLDivElement> {
+export interface HandleProps extends HTMLAttributes<HTMLDivElement> {
   axis: Axis
   hover: boolean
-  height: number
-  width: number
-  onChangeHeight: (newHeight: number) => void
-  onChangeWidth: (newHeight: number) => void
+  height?: number
+  width?: number
+  onChangeHeight?: (newHeight: number) => void
+  onChangeWidth?: (newHeight: number) => void
 }
 
-const CardHandle = forwardRef<HTMLDivElement, CardHandleProps>(
+const Handle = forwardRef<HTMLDivElement, HandleProps>(
   ({className, hover, axis, height, width, onChangeHeight, onChangeWidth, ...props}, ref) => {
     const [isDragging, setIsDragging] = useState<boolean>(false);
     const [mousePos, setMousePos] = useState<{x: number, y: number}>({x: 0, y: 0})
     const resizeBox: MouseEventHandler<HTMLDivElement> = (e) => {
-      if (axis.includes('y')) {
+      if (height && onChangeHeight && axis.includes('y')) {
         const diffY = e.clientY - mousePos.y
         const newHeight = height + diffY;
         onChangeHeight(newHeight)
         const newMousePos = {x: e.clientX, y: e.clientY}
         setMousePos(newMousePos)
       }
-      if (axis.includes('x')) {
+      if (width && onChangeWidth && axis.includes('x')) {
         const diffX = e.clientX - mousePos.x;
         const newWidth = width + diffX;
         onChangeWidth(newWidth)
@@ -81,7 +81,7 @@ const CardHandle = forwardRef<HTMLDivElement, CardHandleProps>(
   }
 )
 
-CardHandle.displayName = 'CardHandle'
+Handle.displayName = 'Handle'
 
 export interface CardProps extends HTMLAttributes<HTMLDivElement> {
   height: number
@@ -90,7 +90,7 @@ export interface CardProps extends HTMLAttributes<HTMLDivElement> {
   onChangeWidth?: Dispatch<SetStateAction<number>>
 }
 
-export const Card = forwardRef<HTMLDivElement, CardProps>(
+export const ResizableCard = forwardRef<HTMLDivElement, CardProps>(
   ({ className, children, height, width, onChangeHeight, onChangeWidth, ...props}, ref) => {
     const [hover, setHover] = useState(false)
     return (
@@ -104,9 +104,26 @@ export const Card = forwardRef<HTMLDivElement, CardProps>(
       >
         {children}
         <MixerHorizontalIcon className={cn(hover ? 'opacity-100' : 'opacity-0', 'transition-opacity duration-700 ease-out absolute top-2 right-2')} />
-        <CardHandle hover={hover} axis='y' height={height} width={width} onChangeHeight={(h) => onChangeHeight ? onChangeHeight(h) : null} onChangeWidth={(w) => onChangeWidth ? onChangeWidth(w) : null} />
-        <CardHandle hover={hover} axis='x' height={height} width={width} onChangeHeight={(h) => onChangeHeight ? onChangeHeight(h) : null} onChangeWidth={(w) => onChangeWidth ? onChangeWidth(w) : null} />
-        <CardHandle hover={hover} axis='xy' height={height} width={width} onChangeHeight={(h) => onChangeHeight ? onChangeHeight(h) : null} onChangeWidth={(w) => onChangeWidth ? onChangeWidth(w) : null} />
+        {onChangeHeight && <Handle hover={hover} axis='y' height={height} onChangeHeight={(h) => onChangeHeight(h)} />}
+        {onChangeWidth && <Handle hover={hover} axis='x' width={width} onChangeWidth={(w) => onChangeWidth(w)} />}
+        {onChangeHeight && onChangeWidth && <Handle hover={hover} axis='xy' height={height} width={width} onChangeHeight={(h) => onChangeHeight(h)} onChangeWidth={(w) => onChangeWidth(w)} />}
+      </div>
+    )
+  }
+)
+
+ResizableCard.displayName = 'ResizableCard'
+
+export const Card = forwardRef<HTMLDivElement, CardProps>(
+  ({ className, children, height, width, ...props}, ref) => {
+    return (
+      <div
+        ref={ref}
+        style={{height: height, width: width}}
+        className={cn('w-60 rounded-lg border bg-card text-card-foreground shadow-sm flex justify-center items-center relative', className)}
+        {...props}
+      >
+        {children}
       </div>
     )
   }
