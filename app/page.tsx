ddb58bf865765ec274f76f636117a9b466242b47
KeyonYan <835254227@qq.com>
2023-12-17 18:30:37 +0800

    🦄 refactor: Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -59,25 +59,25 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-start p-24 gap-5">
       <div className='text-3xl'>React Challenges</div>
       <div className='grid grid-flow-row-dense grid-cols-5 grid-rows-5 gap-4'>
-        <Link href="/challenges/day1">
+        <Link href="/day1">
           <Card className='p-4'>Show / Hide</Card>
         </Link>
-        <Link href="/challenges//day2">
+        <Link href="/day2">
           <Card className='p-4'>Timer</Card>
         </Link>
-        <Link href="/challenges//day3">
+        <Link href="/day3">
           <Card className='p-4'>Todo list</Card>
         </Link>
-        <Link href="/challenges//day5">
+        <Link href="/day5">
           <Card className='p-4'>ProgressBar</Card>
         </Link>
-        <Link href="/challenges//day6">
+        <Link href="/day6">
           <Card className='p-4'>Nextjs Router handler & fetch</Card>
         </Link>
-        <Link href="/challenges//day7">
+        <Link href="/day7">
           <Card className='p-4'>Resizable Card</Card>
         </Link>
-        <Link href="/challenges//day7">
+        <Link href="/day7">
           <Card className='p-4'>Draggable Card</Card>
         </Link>
       </div>
