beac255f74d4c796ba20fb29d2410228b120fa0f
KeyonYan <835254227@qq.com>
2023-12-24 23:10:32 +0800

    ✨ feat: Resizable

--- components/MutableView/MutableView.tsx
+++ components/MutableView/MutableView.tsx
@@ -26,6 +26,9 @@ export const MutableView = forwardRef<HTMLDivElement, MutableViewProps>(
     const h = useSpring(rect.h)
     const x = useMotionValue(rect.x)
     const y = useMotionValue(rect.y)
+    const resizerLen = 15
+    const resizerX = useMotionValue(rect.x + rect.w - resizerLen)
+    const resizerY = useMotionValue(rect.y + rect.h - resizerLen)
     
     useEffect(() => {
       const rootElement = rootRef.current
@@ -40,6 +43,8 @@ export const MutableView = forwardRef<HTMLDivElement, MutableViewProps>(
       h.set(rect.h)
       x.set(rect.x)
       y.set(rect.y)
+      resizerX.set(rect.x + rect.w - resizerLen)
+      resizerY.set(rect.y + rect.h - resizerLen)
     }, [rect])
 
     const handleRectChange = () => {
@@ -51,7 +56,15 @@ export const MutableView = forwardRef<HTMLDivElement, MutableViewProps>(
       }
       onRectChange(newRect)
     }
-    
+    const handleResizer = (e: MouseEvent | TouchEvent | PointerEvent, info: PanInfo) => {
+      const newRect = {
+        x: x.get(),
+        y: y.get(),
+        w: resizerX.get() - x.get() + resizerLen,
+        h: resizerY.get() - y.get() + resizerLen,
+      }
+      onRectChange(newRect)
+    }
     const dragConstraints = {
       left: 0,
       top: 0,
@@ -59,24 +72,50 @@ export const MutableView = forwardRef<HTMLDivElement, MutableViewProps>(
       bottom: mutAreaSize.h-rect.h,
     };
     return (
-      <motion.div 
-        ref={rootRef}
-        className='absolute'
-        style={{
-          x,
-          y,
-          width: w, 
-          height: h
-        }} 
-        whileHover={{ scale: 1.1 }}
-        drag={!fixed}
-        onDrag={handleRectChange}
-        onTransitionEnd={handleRectChange}
-        dragConstraints={dragConstraints}
-        dragMomentum={false}
-      >
-        {children}
-      </motion.div>
+      <>
+        <motion.div
+          ref={rootRef}
+          className='absolute cursor-move'
+          style={{
+            x,
+            y,
+            width: w,
+            height: h
+          }} 
+          whileHover={{ scale: 1.1 }}
+          drag={!fixed}
+          onDrag={handleRectChange}
+          dragConstraints={dragConstraints}
+          dragMomentum={false}
+          dragElastic={false}
+          dragPropagation
+        >
+          {children}
+        </motion.div>
+        
+        {
+          !fixed && 
+          <motion.div
+            className='absolute w-[15px] h-[15px] rounded-md bg-[#6f6f6f] cursor-nwse-resize'
+            drag
+            style={{
+              x: resizerX,
+              y: resizerY
+            }}
+            dragConstraints={{
+              left: 0,
+              top: 0,
+              right: mutAreaSize.w - resizerLen,
+              bottom: mutAreaSize.h - resizerLen,
+            }}
+            onDrag={(e, info) => handleResizer(e, info)}
+            onDragEnd={(e, info) => handleResizer(e, info)}
+            dragMomentum={false}
+            dragElastic={false}
+          >
+          </motion.div>
+        }
+      </>
     )
   }
 )
