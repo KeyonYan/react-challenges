a4752b9524fdb3233a3a892a054e5d7cc8b64650
KeyonYan <835254227@qq.com>
2024-05-24 13:13:47 +0800

    🦄 refactor: app layout

--- app/(challenges)/app-layout/page.tsx
+++ app/(challenges)/app-layout/page.tsx
@@ -1,20 +1,7 @@
 'use client'
 
 import AppLayout from "@/components/AppLayout";
-import { cn } from "@/lib/utils";
-import { useMouse } from "@uidotdev/usehooks";
-import { motion, MotionProps } from "framer-motion";
 import { atom, useAtom } from "jotai";
-import { forwardRef, useRef, useState } from "react";
-
-const BASELEN = 50, GAP = 20
-
-interface Comp {
-  row: number,
-  col: number,
-  width: number,
-  height: number
-}
 
 const comps = atom([
   {
@@ -33,99 +20,12 @@ const comps = atom([
     width: 2,
     height: 1,
   },
-
 ])
 
-
-interface DragShadowProps extends MotionProps {
-  row?: number,
-  col?: number,
-  width?: number,
-  height?: number,
-  className?: string
-}
-
-export const DragShadow = forwardRef<HTMLDivElement, DragShadowProps>(
-  ({row = 0, col = 0, width = 0, height = 0, className, ...props}, ref) => {
-    return (
-      <motion.div
-        ref={ref}
-        style={{
-          top: BASELEN*row + (row+1)*GAP, 
-          left: BASELEN*col + (col+1)*GAP,
-          width: BASELEN*width+((width-1)*GAP),
-          height: BASELEN*height+((height-1)*GAP)
-        }}
-        className={`absolute bg-transparent rounded-lg border z-0`}
-        {...props}
-      >
-      </motion.div>
-    )
-  }
-)
-
-DragShadow.displayName = 'DragShadow'
-
-interface CompProps extends MotionProps {
-  width: number,
-  height: number,
-  row: number
-  col: number,
-  className?: string,
-}
-
-export function Comp({width, height, row, col, className, children, ...props}: CompProps) {
-  return (
-    <motion.div
-      initial={{
-        width: BASELEN*width+((width-1)*GAP), 
-        height: BASELEN*height+((height-1)*GAP),
-      }}
-      drag
-      whileHover={{scale: 1.1}}
-      style={{
-        top: BASELEN*row + (row+1)*GAP,
-        left: BASELEN*col + (col+1)*GAP
-      }}
-      className={cn(className, `absolute z-1 bg-cyan-100 rounded-lg border shadow-sm flex justify-center items-center`)}
-      {...props}
-    >
-        {children}
-    </motion.div>
-  )
-}
-
 export default function AppLayoutPage() {
-  const [shadow, setShadow] = useState<DragShadowProps>()
   const [compsValue, setCompsValue] = useAtom(comps)
-  const [mouse, layoutRef] = useMouse<HTMLDivElement>()
-  const shadowRef = useRef<HTMLDivElement>(null)
-  const [isDrag, setIsDrag] = useState(false)
-  function handleDrag(e: DragEvent, item: CompProps) {
-    const n = Math.floor((mouse.elementY - GAP) / (BASELEN + GAP))
-    const m = Math.floor((mouse.elementX - GAP) / (BASELEN + GAP))
-    setShadow({row: n, col: m, width: item.width, height: item.height})
-  }
-  function handleDragEnd(e: DragEvent) {
-    setIsDrag(false)
-  }
   return (
-    <AppLayout ref={layoutRef} className="relative w-[800px] h-[800px] m-10 border">
-      <DragShadow ref={shadowRef} {...shadow} />
-      {compsValue.map((item, index) => {
-        return (
-          <Comp
-            {...item}
-            key={index}
-            onDragStart={() => setIsDrag(true)}
-            onDrag={(e) => handleDrag(e as DragEvent, item)}
-            onDragEnd={handleDragEnd}
-            dragConstraints={shadowRef}
-          >
-            *
-          </Comp>
-        )
-      })}
+    <AppLayout comps={compsValue} gap={20} unit={50} className="relative w-[800px] h-[800px] m-10 border">
     </AppLayout>
   )
 }
--- components/AppLayout/index.tsx
+++ components/AppLayout/index.tsx
@@ -1,15 +1,47 @@
 import { cn } from "@/lib/utils";
-import { forwardRef, HTMLAttributes } from "react";
+import { useMouse } from "@uidotdev/usehooks";
+import { motion, MotionProps } from "framer-motion";
+import { forwardRef, HTMLAttributes, useRef, useState } from "react";
+
 
 interface AppLayoutProps extends HTMLAttributes<HTMLDivElement> {
-  
+  comps: Comp[]
+  unit: number,
+  gap: number,
 }
 
 export const AppLayout = forwardRef<HTMLDivElement, AppLayoutProps>(
-  ({className, children, ...props}, ref) => {
+  ({comps, unit, gap, className, children, ...props}, ref) => {
+    const [shadow, setShadow] = useState<DragShadowProps>()
+    const [compsValue, setCompsValue] = useState(comps)
+    const [mouse, layoutRef] = useMouse<HTMLDivElement>()
+    const shadowRef = useRef<HTMLDivElement>(null)
+    const [isDrag, setIsDrag] = useState(false)
+    function handleDrag(e: DragEvent, item: CompProps) {
+      const n = Math.floor((mouse.elementY - gap) / (unit + gap))
+      const m = Math.floor((mouse.elementX - gap) / (unit + gap))
+      setShadow({row: n, col: m, width: item.width, height: item.height})
+    }
+    function handleDragEnd(e: DragEvent) {
+      setIsDrag(false)
+    }
     return (
-      <div ref={ref} className={cn(className)}>
-        {children}
+      <div ref={layoutRef} className={cn(className, "relative w-[800px] h-[800px] m-10 border")}>
+        <DragShadow ref={shadowRef} {...shadow} />
+        {compsValue.map((item, index) => {
+          return (
+            <Comp
+              {...item}
+              key={index}
+              onDragStart={() => setIsDrag(true)}
+              onDrag={(e) => handleDrag(e as DragEvent, item)}
+              onDragEnd={handleDragEnd}
+              dragConstraints={shadowRef}
+            >
+              *
+            </Comp>
+          )
+        })}
       </div>
     )
   }
@@ -18,3 +50,70 @@ export const AppLayout = forwardRef<HTMLDivElement, AppLayoutProps>(
 AppLayout.displayName = 'AppLayout'
 
 export default AppLayout
+
+const unit = 50, gap = 20
+
+interface Comp {
+  row: number,
+  col: number,
+  width: number,
+  height: number
+}
+
+interface DragShadowProps extends MotionProps {
+  row?: number,
+  col?: number,
+  width?: number,
+  height?: number,
+  className?: string
+}
+
+const DragShadow = forwardRef<HTMLDivElement, DragShadowProps>(
+  ({row = 0, col = 0, width = 0, height = 0, className, ...props}, ref) => {
+    return (
+      <motion.div
+        ref={ref}
+        style={{
+          top: unit*row + (row+1)*gap, 
+          left: unit*col + (col+1)*gap,
+          width: unit*width+((width-1)*gap),
+          height: unit*height+((height-1)*gap)
+        }}
+        className={`absolute bg-transparent rounded-lg border z-0`}
+        {...props}
+      >
+      </motion.div>
+    )
+  }
+)
+
+DragShadow.displayName = 'DragShadow'
+
+interface CompProps extends MotionProps {
+  width: number,
+  height: number,
+  row: number
+  col: number,
+  className?: string,
+}
+
+export function Comp({width, height, row, col, className, children, ...props}: CompProps) {
+  return (
+    <motion.div
+      initial={{
+        width: unit*width+((width-1)*gap), 
+        height: unit*height+((height-1)*gap),
+      }}
+      drag
+      whileHover={{scale: 1.1}}
+      style={{
+        top: unit*row + (row+1)*gap,
+        left: unit*col + (col+1)*gap
+      }}
+      className={cn(className, `absolute z-1 bg-cyan-100 rounded-lg border shadow-sm flex justify-center items-center`)}
+      {...props}
+    >
+        {children}
+    </motion.div>
+  )
+}
