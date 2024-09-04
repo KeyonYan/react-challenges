d40918b1c148a9505e4f768511f6937693b5aba8
yankeyu <yankeyu@xiaomi.com>
2024-09-04 23:27:45 +0800

    feat: react flow

--- app/(challenges)/xyflow/page.tsx
+++ app/(challenges)/xyflow/page.tsx
@@ -1,45 +1,90 @@
 'use client'
+import Dagre from '@dagrejs/dagre';
 import React, { useCallback } from 'react';
 import {
   ReactFlow,
-  MiniMap,
-  Controls,
-  Background,
+  ReactFlowProvider,
+  Panel,
   useNodesState,
   useEdgesState,
-  addEdge,
+  useReactFlow,
 } from '@xyflow/react';
 
+import { initialNodes, initialEdges } from './nodes-edges';
 import '@xyflow/react/dist/style.css';
+import { Button } from '@/components/ui/button';
 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const getLayoutedElements = (nodes, edges, options) => {
+  const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+  g.setGraph({ rankdir: options.direction });
 
-export default function XyFlowPage() {
+  edges.forEach((edge) => g.setEdge(edge.source, edge.target));
+  nodes.forEach((node) =>
+    g.setNode(node.id, {
+      ...node,
+      width: node.measured?.width ?? 0,
+      height: node.measured?.height ?? 0,
+    }),
+  );
+
+  Dagre.layout(g);
+
+  return {
+    nodes: nodes.map((node) => {
+      const position = g.node(node.id);
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the React Flow node anchor point (top left).
+      const x = position.x - (node.measured?.width ?? 0) / 2;
+      const y = position.y - (node.measured?.height ?? 0) / 2;
+
+      return { ...node, position: { x, y } };
+    }),
+    edges,
+  };
+};
+
+const LayoutFlow = () => {
+  const { fitView } = useReactFlow();
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges],
+  const onLayout = useCallback(
+    (direction) => {
+      console.log(nodes);
+      const layouted = getLayoutedElements(nodes, edges, { direction });
+
+      setNodes([...layouted.nodes]);
+      setEdges([...layouted.edges]);
+
+      window.requestAnimationFrame(() => {
+        fitView();
+      });
+    },
+    [nodes, edges],
   );
 
   return (
-    <div style={{ width: '90vw', height: '90vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-      >
-        <Controls />
-        <MiniMap />
-        <Background variant="dots" gap={12} size={1} />
-      </ReactFlow>
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      fitView
+    >
+      <Panel position="top-left" className='flex flex-col gap-2'>
+        <Button onClick={() => onLayout('TB')}>vertical layout</Button>
+        <Button onClick={() => onLayout('LR')}>horizontal layout</Button>
+      </Panel>
+    </ReactFlow>
+  );
+};
+
+export default function XyFlowPage() {
+  return (
+    <div className='w-[100vw] h-[100vh]'>
+      <ReactFlowProvider>
+        <LayoutFlow />
+      </ReactFlowProvider>
     </div>
-  )
+  );
 }
