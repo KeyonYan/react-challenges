6cf944e58bd40374c7b72cb07f77507cdfab9c0b
KeyonYan <835254227@qq.com>
2024-09-08 13:58:16 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,14 @@ const nextConfig = {
       }
     ]
   }
+  eslint: {
+      ignoreDuringBuilds: true, // 忽略 eslint 检查
+  },
+  typescript: {
+      ignoreBuildErrors: true, // 忽略 TypeScript 检查
+  }
+}
+
 }
 
 module.exports = nextConfig
