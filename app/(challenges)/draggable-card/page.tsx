d6ac298930bda599663827957723116c10a0d1f4
yankeyu <yankeyu@xiaomi.com>
2024-09-09 23:17:35 +0800

    build: command

--- app/(challenges)/draggable-card/page.tsx
+++ app/(challenges)/draggable-card/page.tsx
@@ -1,21 +1,24 @@
 'use client'
 import { DraggableArea, DraggableBox } from "@/components/DraggableBox";
 import { ResizableCard, CardSize } from "@/components/ResizableCard";
+import { CommandShortcut } from "@/components/ui/command";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
 
 export default function DraggableCardPage() {
-  const [firstCardSize, setFirstCardSize] = useState<CardSize>({h: 300, w: 200})
-  const [secondCardSize, setSecondCardSize] = useState<CardSize>({h: 300, w: 200})
+  const [firstCardSize, setFirstCardSize] = useState<CardSize>({ h: 300, w: 200 })
+  const [secondCardSize, setSecondCardSize] = useState<CardSize>({ h: 300, w: 200 })
   return (
     <DraggableArea className='w-[1000px] h-[500px] rounded-lg border shadow-sm'>
+      <span>Frame Select  </span>
+      <CommandShortcut>Ctrl+Mouse1</CommandShortcut>
       <DraggableBox>
-        <ResizableCard size={firstCardSize} min={{h: 60, w: 60}} onChangeSize={setFirstCardSize} className='bg-red-100'>
+        <ResizableCard size={firstCardSize} min={{ h: 60, w: 60 }} onChangeSize={setFirstCardSize} className='bg-red-100'>
           <GitHubLogoIcon className='w-10 h-10' />
         </ResizableCard>
       </DraggableBox>
       <DraggableBox>
-        <ResizableCard size={secondCardSize} min={{h: 60, w: 60}} onChangeSize={setSecondCardSize} className='text-4xl bg-blue-100'>
+        <ResizableCard size={secondCardSize} min={{ h: 60, w: 60 }} onChangeSize={setSecondCardSize} className='text-4xl bg-blue-100'>
           😁
         </ResizableCard>
       </DraggableBox>
