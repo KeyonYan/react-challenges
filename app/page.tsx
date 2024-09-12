6ca9b5415bb19752f26730e643c1b8ba0111f190
yankeyu <yankeyu@xiaomi.com>
2024-09-13 00:37:34 +0800

    feat: index title animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,48 @@
 'use client'
 import Link from 'next/link'
 import { Card } from '@/components/Card'
+import { LogosReactMotion } from './icons/Icons'
+import { motion, useAnimate } from 'framer-motion'
+import { useState } from 'react'
 
 export default function Home() {
+  const [scope, animate] = useAnimate()
+  const [speed, setSpeed] = useState(1)
+  const handleIconClick = () => {
+    animate(scope.current, { rotate: 360 }, { duration: 1.5 / speed })
+      .then(() => {
+        animate(scope.current, { rotate: 0 }, { duration: 0 })
+      })
+    setSpeed(speed => speed + 0.1)
+  }
+  const animateString = (str: string) => {
+    return str.split('').map((rune, index) => {
+      return {
+        rune: rune,
+        initial: { opacity: 0 },
+        animate: { opacity: 1 },
+        transition: { delay: index * 0.1 }
+      }
+    })
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24 gap-5">
-      <div className='text-3xl'>React Challenges</div>
-      <div className='grid grid-flow-row-dense grid-cols-5 grid-rows-5 gap-4'>
+      <h1 className='text-2xl select-none flex flex-col items-center gap-3'>
+        <div className='flex flex-row items-center gap-3'>
+          <div className='flex gap-1'>
+            {animateString('React').map((el, i) => {
+              return <motion.span className='p-0' key={String(i)} {...el} transition={{ delay: i * 0.1 }}>{el.rune}</motion.span>
+            })}
+          </div>
+          <LogosReactMotion ref={scope} onClick={handleIconClick} className='overflow-visible w-12 h-12' />
+        </div>
+        <div className='flex gap-1'>
+          {animateString('Challenge').map((el, i) => {
+            return <motion.span key={String(i)} {...el} transition={{ delay: i * 0.1 }}>{el.rune}</motion.span>
+          })}
+        </div>
+      </h1>
+      <div className='grid grid-flow-row-dense sm:grid-cols-1 md:grid-cols-5 grid-rows-5 gap-4'>
         <Link href="/timer">
           <Card className='p-4'>Timer</Card>
         </Link>
